46656cc4ebd4c1be93c7b80188521beb0e49e59d
FlorianMichael <florian.michael07@gmail.com>
2025-10-08 16:36:29 +0200

    Don't include Fabric API modules

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,7 +7,6 @@ plugins {
 
 setupProject()
 setupFabric()
-includeFabricApiModules("fabric-api-base", "fabric-resource-loader-v0", "fabric-screen-api-v1", "fabric-key-binding-api-v1", "fabric-lifecycle-events-v1")
 
 repositories {
     maven("https://maven.terraformersmc.com/releases")
